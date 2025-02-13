b768c26d2aadafd376690360a3f7c96edff256e9
Eric Venturino <eric.venturino@orange.fr>
2025-02-13 15:53:08 +0100

    raz

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['firebasestorage.googleapis.com'], // Ajoutez le domaine de Firebase ici
-  },
-};
-
-export default nextConfig;
